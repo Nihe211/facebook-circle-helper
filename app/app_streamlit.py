61c4145eefc0dd7b9b3819ba9079712dc9868f8b
Nihe211 <thanhhaoao213@gmail.com>
2025-11-16 16:14:55 +0700

    Update code in app_streamlit.py

--- app/app_streamlit.py
+++ app/app_streamlit.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from pathlib import Path
+import sys
+from pathlib import Path
+ROOT = Path(__file__).resolve().parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
 import pandas as pd
 
 from src.data_loader import load_ego_graph
